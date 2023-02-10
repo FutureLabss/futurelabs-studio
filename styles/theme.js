ec1433509d0746b1bb9838b8da840f250b2915b4
Usenmfon <usenmfonuko@gmail.com>
2023-02-10 16:26:34 +0100

    added scrollbar

--- styles/theme.js
+++ styles/theme.js
@@ -42,18 +42,7 @@ let theme = {
             }
           }
         }
-     },'@global': {
-        '*::-webkit-scrollbar': {
-          width: '0.4em'
-        },
-        '*::-webkit-scrollbar-track': {
-          '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
-        },
-        '*::-webkit-scrollbar-thumb': {
-          backgroundColor: 'rgba(0,0,0,.1)',
-          outline: '1px solid slategrey'
-        }
-      }
+     },
 }
 
 theme = createTheme(theme)
