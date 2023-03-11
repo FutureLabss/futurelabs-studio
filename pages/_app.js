12a06b39be7ff63f481cd7c3dbe808aa0cd72481
Usenmfon <usenmfonuko@gmail.com>
2023-03-11 11:39:25 +0100

    added head tag to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import "@fontsource/inter"
 import { ThemeProvider } from '@mui/material'
+import Head from 'next/head'
 import theme from "../styles/theme"
 import createEmotionCache from '../styles/createEmotionCache'
 import { CacheProvider } from '@emotion/react'
@@ -10,6 +11,12 @@ const clientSideEmotionCache = createEmotionCache();
 function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
   return (
     <CacheProvider value={emotionCache}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="title, meta, nextjs" /> 
+        <meta name="viewport" content="width=device-width, intial-scale=1.0" /> 
+        <title>Futurelabs Studio</title>
+      </Head>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
