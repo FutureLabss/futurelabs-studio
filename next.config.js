f8f8b08530cd43f43c263dd494d7942cf7b806f3
Usenmfon <usenmfonuko@gmail.com>
2023-03-11 10:27:42 +0100

    added extra layer of security

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+]
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -8,6 +28,15 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = {fs: false}
     return config
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
